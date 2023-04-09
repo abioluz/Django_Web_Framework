a204edbe70351f0edf7620a9d9817dbd1cb2a424
Abioluz <abioluz@gmail.com>
2023-04-09 16:07:10 -0300

    Configurando VS CODE

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,15 +1,6 @@
-
 from django.urls import path
-
 from recipes.views import home
-from recipes.views import contato
-from recipes.views import sobre
-
-
-
 
 urlpatterns = [
-
     path('', home),
-    path('sobre/', sobre),
-    path('contato', contato),]
+]
--- recipes/views.py
+++ recipes/views.py
@@ -4,12 +4,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'recipes/home.html',{
+    return render(request,'recipes/pages/home.html',{
         'name':'Abioluz Robson'
     })
 
-def sobre(request):
-    return HttpResponse('Sobre'.upper())
 
-def contato(request):
-    return HttpResponse('contato'.upper())
+#
