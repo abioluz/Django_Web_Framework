0047ff9bbf90d40ae84a9c1b581441aa7342313a
Abioluz <abioluz@gmail.com>
2023-04-11 22:17:38 -0300

    seção 7 aula 49

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 def home(request):
     return render(request,'recipes/pages/home.html',{
-        'name':'Abioluz Robson'
+        'recipes':[make_recipe() for _ in range(10)],
+        'is_detail_page': False,
     })
 
 def recipe(request, id):
     return render(request,'recipes/pages/recipe-view.html',{
-        'name':'Abioluz Robson'
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
 #
