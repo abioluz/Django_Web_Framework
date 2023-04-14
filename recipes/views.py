006fec9aecec185d98606479f435a2df765329ff
Abioluz <abioluz@gmail.com>
2023-04-13 23:02:17 -0300

    seção 8 aula 58

--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
-from utils.recipes.factory import make_recipe
+from . import models
+
 
 # Create your views here.
 def home(request):
+    recipes = models.Recipe.objects.all().order_by('-id')
+
     return render(request,'recipes/pages/home.html',{
-        'recipes':[make_recipe() for _ in range(10)],
-        'is_detail_page': False,
+        'recipes' : recipes
+    })
+
+def categoy(request, category_id):
+    recipes = models.Recipe.objects.filter(category__id=category_id).order_by('-id')
+
+    return render(request,'recipes/pages/home.html',{
+        'recipes' : recipes
     })
 
 def recipe(request, id):
+    recipes = models.Recipe.objects.get(id=id)
     return render(request,'recipes/pages/recipe-view.html',{
-        'recipe': make_recipe(),
+        'recipe': recipes,
         'is_detail_page': True,
     })
 
-#
