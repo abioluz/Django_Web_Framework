3e67fd9c6d456797edf92530e85bf9618bc9c795
Abioluz <abioluz@gmail.com>
2023-04-18 23:27:50 -0300

    Aula 75

--- recipes/tests.py
+++ recipes/tests.py
@@ -2,14 +2,28 @@ from django.test import TestCase
 from django.urls import reverse
 
 # Create your tests here.
+# pip install pytest pytest-django
+# pip install pytest-watch
+# ptw para ficar gerando o teste de forma automática
 
-class RecipeURLsTest(TestCase):
+
+class RecipeURLsOK(TestCase):
     # def test_the_pytest_is_ok(self):
     #     print('olá mundo')
-    #     assert  2 == 2
-    # pip install pytest-watch
-    # ptw para ficar gerando o teste de forma automática
+    #     assert  2 == 3
 
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', args=(1,))
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_category_url_is_correct2(self):
+        url = reverse('recipes:category', kwargs={'category_id': 1})
+        self.assertEqual(url, '/recipes/category/1/')
+
+    def test_recipe_detail_url_is_correct(self):
+        url = reverse('recipes:recipe', kwargs={'id': 1})
+        self.assertEqual(url, '/recipes/1/')
--- recipes/views.py
+++ recipes/views.py
@@ -1,44 +1,43 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
 from . import models
 
-
-
 # Create your views here.
+
+
 def home(request):
     recipes = models.Recipe.objects.filter(is_published=True).order_by('-id')
-    
     # recipes = get_list_or_404(
     #     models.Recipe.objects.filter(
     #     is_published=True).order_by('-id'))
-    
-    return render(request,'recipes/pages/home.html',{
-        'recipes' : recipes
+ 
+    return render(request, 'recipes/pages/home.html', {
+        'recipes': recipes
     })
+
 
 def category(request, category_id):
     # recipes = models.Recipe.objects.filter(
-    #     is_published=True, 
-    #     category__id=category_id).order_by('-id')
+    #     is_published=True, category__id=category_id).order_by('-id')
     # if not recipes:
     #     raise Http404 ('Not Found - Pena que não existe')
 
     recipes = get_list_or_404(
         models.Recipe.objects.filter(
-        is_published=True, 
-        category__id=category_id).order_by('-id')
+            is_published=True,
+            category__id=category_id).order_by('-id')
     )
 
-    return render(request,'recipes/pages/category.html',{
-        'recipes' : recipes,
+    return render(request, 'recipes/pages/category.html', {
+        'recipes': recipes,
         'title': f'{recipes[0].category.name}  - Category |'
     })
+
 
 def recipe(request, id):
     # recipes = models.Recipe.objects.get(is_published=True,id=id)
 
-    recipes = get_object_or_404(models.Recipe, is_published=True,id=id)
-    return render(request,'recipes/pages/recipe-view.html',{
+    recipes = get_object_or_404(models.Recipe, is_published=True, id=id)
+    return render(request, 'recipes/pages/recipe-view.html', {
         'recipe': recipes,
         'is_detail_page': True,
     })
-
