aef98c2db3e176899b1b452f1c573b9834f093d8
Abioluz <abioluz@gmail.com>
2023-04-19 22:02:55 -0300

    Aula 76

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.urls import resolve
+from . import views
 
 # Create your tests here.
 # pip install pytest pytest-django
@@ -7,7 +9,7 @@ from django.urls import reverse
 # ptw para ficar gerando o teste de forma automática
 
 
-class RecipeURLsOK(TestCase):
+class RecipeURLsTest(TestCase):
     # def test_the_pytest_is_ok(self):
     #     print('olá mundo')
     #     assert  2 == 3
@@ -27,3 +29,18 @@ class RecipeURLsOK(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve('/')
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_views_function_is_correct(self):
+        view = resolve('/recipes/category/1/')
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_recipes_views_function_is_correct(self):
+        view = resolve('/recipes/1/')
+        self.assertIs(view.func, views.recipe)
